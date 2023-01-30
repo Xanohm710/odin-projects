3aacd36be75ea0f3e5e9bdf15c04af8247865d4e
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2023-01-30 11:56:20 -0500

    Odin JS Fundamentals Part 1 Assignment

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// JS Fundamentals Part 1 assignment
+
+console.log(29 + 30);
+
+let x = (4+6+9)/77;
+console.log(x);
+
+let a = 10;
+let b = 7 * a;
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+const erin = 29;
+const jared = 30;
+let ageDiff = jared % erin;
+console.log(ageDiff);
